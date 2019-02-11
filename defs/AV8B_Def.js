a7e1a66fcc8657dddcaf975d3ea1f39919732cb4
RobotSpaceBear <47452069+RobotSpaceBear@users.noreply.github.com>
2019-02-11 14:04:44 +0100

    added some harrier weight definitions

---
+++ defs/AV8B_Def.js
@@ -0,0 +1,10 @@
+/* weights */
+
+//all weights are in lb
+const airframe_weight = 13537;
+//const max_to_weight = 31086; // not sure about this one
+const max_landing_weight = 26000;
+const max_weight = 31086;
+const max_vertical_to_weight = 20500;
+
+/* pylons */
