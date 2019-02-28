3398bce8d06c2362e5484b4cd97a7b00b6184975
RobotSpaceBear <47452069+RobotSpaceBear@users.noreply.github.com>
2019-02-28 15:50:12 +0100

    added weapons def file

---
+++ defs/weapon_defs.js
@@ -0,0 +1,51 @@
+var weapons = {
+    bombs: {
+        mk82: {
+            "name": "Mk-82",
+            "info": "General purpose unguided bomb",
+            "weight": 241,
+            "emptyWeight": 0
+        },
+        mk84: {
+            "name": "Mk-84",
+            "info": "General purpose unguided bomb",
+            "weight": 894,
+            "emptyWeight": 0
+        }
+    },
+    missiles: {
+        agm65e: {
+            "name": "AGM-65E Maverick",
+            "info": "Medium-range, laser-guided, air-to-surface missile",
+            "weight": 293
+        },
+        agm65g: {
+
+        }
+    },
+    pods: {
+    },
+    rockets: {
+    }
+};
+
+//use examples
+/*
+for (var bomb in weapons.bombs) {
+
+}
+
+weapons.?.mk82;
+
+for (var type in weapons) {
+    if (weapons[type][truc_recherche]) {
+
+    }
+}
+
+
+weapons.type.truc_recherche
+
+var myType = 'bomb';
+weapons[myType][truc_recherche]
+*/
